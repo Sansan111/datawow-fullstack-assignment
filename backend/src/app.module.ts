470cb7f0ba867e6bd83a33be94b36c7ee3c893b3
Sansan111 <shannavathon@gmail.com>
2026-05-07 01:08:22 +0700

    generate api resource for auth concerts and reservations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ConcertsModule } from './concerts/concerts.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, ConcertsModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
