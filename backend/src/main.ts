e44d6956e470d32bb0faadadc6ded0c5517641a8
Sansan111 <shannavathon@gmail.com>
2026-05-08 01:05:46 +0700

    update database local port and change prisma setup config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  })); // use only field that are in the dto
-  await app.listen(process.env.PORT ?? 3000);
-  await app.listen(3000);
-  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  const port = Number(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(port);
+  Logger.log(`Server is running on port ${port}`, 'Bootstrap');
 }
 bootstrap();
