21680c3ae28af58f0c7f4f6d7b97ec6f4119c98f
Sansan111 <shannavathon@gmail.com>
2026-05-08 00:47:14 +0700

    implement auth register and login jwt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) { }
+  // register new user
+  async register(dto: AuthDto) {
+    console.log('Registering user...');
+    const userExists = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (userExists) throw new BadRequestException('Email already in use');
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+      },
+    });
+
+    return this.signToken(user.id, user.email, user.role);
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  // login user
+  async login(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) throw new UnauthorizedException('Credentials incorrect');
+
+    const pwMatches = await bcrypt.compare(dto.password, user.password);
+    if (!pwMatches) throw new UnauthorizedException('Credentials incorrect');
+
+    return this.signToken(user.id, user.email, user.role);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private signToken(userId: number, email: string, role: string) {
+    const payload = { sub: userId, email, role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
+
