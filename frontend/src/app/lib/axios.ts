29cf33a5ac401850a46380261e5ff7ac815f468c
Sansan111 <shannavathon@gmail.com>
2026-05-09 12:47:35 +0700

    initial frontend project and config axios

---
+++ frontend/src/app/lib/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  // point to backend
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 5000, 
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+export default axiosInstance;
